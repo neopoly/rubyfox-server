50559ae02e654e89ad06641512ccbb9d8e79e72f
Peter Suschlik <ps@neopoly.de>
2012-10-31 11:05:56 +0100

    Extract env into own file.

--- lib/rubyfox/sfs/cli.rb
+++ lib/rubyfox/sfs/cli.rb
@@ -2,6 +2,7 @@ require 'thor'
 require 'mime/types'
 
 require 'rubyfox/sfs'
+require 'rubyfox/sfs/environment'
 
 module Rubyfox
   module SFS
@@ -47,35 +48,7 @@ module Rubyfox
 
       no_tasks do
         def env
-          @env ||= Env.new(ENV)
-        end
-      end
-
-      class Env
-        include Thor::Shell
-
-        def initialize(env)
-          @env = env
-          @local = {}
-        end
-
-        def [](name)
-          name = name.to_s
-          value = @local[name] || @env[name]
-          value || begin
-            msg = "Specify env #{name}:"
-            loop do
-              answer = ask(msg)
-              unless answer.empty?
-                self[name] = answer
-                break answer
-              end
-            end
-          end
-        end
-
-        def []=(name, value)
-          @local[name.to_s] = value
+          @env ||= Environment.new(ENV)
         end
       end
     end
---
+++ lib/rubyfox/sfs/environment.rb
@@ -0,0 +1,31 @@
+module Rubyfox
+  module SFS
+    class Environment
+      include Thor::Shell
+
+      def initialize(env)
+        @env = env
+        @local = {}
+      end
+
+      def [](name)
+        name = name.to_s
+        value = @local[name] || @env[name]
+        value || begin
+          msg = "Specify env #{name}:"
+          loop do
+            answer = ask(msg)
+            unless answer.empty?
+              self[name] = answer
+              break answer
+            end
+          end
+        end
+      end
+
+      def []=(name, value)
+        @local[name.to_s] = value
+      end
+    end
+  end
+end
