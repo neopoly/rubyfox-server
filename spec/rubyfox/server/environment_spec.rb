e09eada62313b72092a5973868fcdc42e5c348f8
Peter Suschlik <ps@neopoly.de>
2014-10-29 13:12:14 +0100

    Avoid stubbing by testing against FakeEnvironment

--- spec/rubyfox/server/environment_spec.rb
+++ spec/rubyfox/server/environment_spec.rb
@@ -2,9 +2,25 @@ require 'spec_helper'
 
 require 'rubyfox/server/environment'
 
+class FakeEnvironment < Rubyfox::Server::Environment
+  attr_reader :answers, :asked
+
+  def initialize(*)
+    super
+    @asked = []
+    @answers = []
+  end
+
+  def ask(message)
+    @asked << message
+    @answers.shift or fail "no answers left"
+  end
+end
+
 describe Rubyfox::Server::Environment do
+
   let(:env) { Hash.new }
-  let(:environment) { Rubyfox::Server::Environment.new(env) }
+  let(:environment) { FakeEnvironment.new(env) }
 
   context "[]" do
     it "returns predefined value" do
@@ -13,36 +29,33 @@ describe Rubyfox::Server::Environment do
     end
 
     it "asks for missing value" do
-      with_stdin do |stdin|
-        stdin << "23\n"
-        assert_output "Specify env foo: " do
-          assert_equal "23", environment["foo"]
-        end
-      end
+      environment.answers << "23"
 
       assert_equal "23", environment["foo"]
+      assert_equal "23", environment["foo"]
+
+      assert_asked "foo", 1
     end
 
     it "keep asking for empty values" do
-      with_stdin do |stdin|
-        expected_output = (["Specify env foo: "] * 2).join
-        assert_output expected_output do
-          stdin << "\n"
-          stdin << "23\n"
-          assert_equal "23", environment["foo"]
-        end
-      end
+      environment.answers << ""
+      environment.answers << "23"
+
+      assert_equal "23", environment["foo"]
+      assert_asked "foo", 2
     end
   end
 
   context "[]=" do
     it "sets env" do
       environment["foo"] = 23
+
       assert_equal 23, environment["foo"]
     end
 
     it "stringifies when setting env" do
       environment[:foo] = 23
+
       assert_equal 23, environment["foo"]
       assert_equal 23, environment[:foo]
     end
@@ -50,19 +63,10 @@ describe Rubyfox::Server::Environment do
 
   private
 
-  def with_stdin
-    line_editor = Thor::LineEditor
-    old_method = line_editor.method(:readline)
-    stdin = []
-    line_editor.define_singleton_method(:readline) do |message, *|
-      $stdout.print message
-      stdin.shift or fail "no input left"
-    end
-    yield stdin
-  ensure
-    line_editor.define_singleton_method(:readline) do |*args|
-      old_method.call(*args)
-    end
+  def assert_asked(key, amount=1)
+    expected = ["Specify env #{key}:"] * amount
+
+    assert_equal expected, environment.asked
   end
 
 end
